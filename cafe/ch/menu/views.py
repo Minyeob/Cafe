53d285c52e99aa7bff8224c79947cfc33c2be52f
min2 <nmy2511@gmail.com>
2016-08-23 16:05:59 +0000

    dict to json

--- cafe/ch/menu/views.py
+++ cafe/ch/menu/views.py
@@ -6,34 +6,21 @@ from django.http import HttpResponse, JsonResponse
 def index(request):
     cafe_list = Cafe.objects.all()
     datalist={}
+    coffeelist={}
     for cafe in cafe_list:
-        for coffee in cafe.coffee_set.all:
-            datalist.update
-            (
-                    {
-                    cafe.cafe_name:
-                        {
-                            'data':
-                                {
-                                'cafe_name':cafe.cafe_name,
-                                'cafe_color':cafe.cafe_color,
-                                'cafe_menu':
-                                    {
-                                    coffee_name:
-                                            {
-                                            'coffee_name':coffee.coffee_name,
-                                            'coffee_price':coffee.coffee_price
-                                            }
+        for coffee in cafe.coffee_set.all():
+            coffeelist.update({coffee.coffee_name: coffee.coffee_price})
+        datalist.update(
+                            {
+                                cafe.cafe_name: {
+                                    'data': {
+                                        'cafe_name':cafe.cafe_name,'cafe_color':cafe.cafe_color, 'cafe_menu':coffeelist
                                     }
                                 }
-                        }
-                    
-                    }
-            )
-    #for cafe in cafe_list:
-        #data.update({cafe.cafe_name: {'data': {'cafe_name':cafe.cafe_name,'cafe_color':cafe.cafe_color}}})
-    #json_data = {'data':data} 
-    return HttpResponse(json.dumps(datalst), content_type='application/json')
+                            }
+                        )
+        #coffeelist={}
+    return HttpResponse(json.dumps(datalist), content_type='application/json')
 
 
 
